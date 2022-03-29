ecace9775ce4340478e99651177479c249e7bbee
bayulee <bayuleepaul@gmail.com>
2022-03-29 14:38:19 +0000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div>
+    <h1>WELCOME TO MY HOMEPAGE</h1>
+    <div>
+    <p>Noto is a global font collection for writing in all modern and ancient languages.
+     Noto Serif is a modulated (“serif”) design for texts in the Latin, Cyrillic and Greek scripts,
+      also suitable as the complementary font for other script-specific Noto Serif fonts.
+     It has italic styles, multiple weights and widths, and 3,256 glyphs.
+     </p>
+     <p>Noto is a global font collection for writing in all modern and ancient languages. 
+     Noto Serif is a modulated (“serif”) design for texts in the Latin, Cyrillic and Greek scripts,
+      also suitable as the complementary font for other script-specific Noto Serif fonts. 
+     It has italic styles, multiple weights and widths, and 3,256 glyphs.</p>
+
+     <p>Noto is a global font collection for writing in all modern and ancient languages. 
+     Noto Serif is a modulated (“serif”) design for texts in the Latin, Cyrillic and Greek scripts,
+      also suitable as the complementary font for other script-specific Noto Serif fonts. 
+     It has italic styles, multiple weights and widths, and 3,256 glyphs.</p>
+    </div>
+    
+    
+    
+    </div>
+  )
+}
+
+export default withLayout(Home)
