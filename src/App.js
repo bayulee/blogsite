405b087dd68af41fe9b148455c99dd9fa500a339
bayulee <bayuleepaul@gmail.com>
2022-04-01 12:37:59 +0000

    added use Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './pages/Home'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/blog' element={<Blog/>}/>
+    <Route path='/about' element={<Navigate to="/"/>}/>
+
   </Routes>
     
   )
